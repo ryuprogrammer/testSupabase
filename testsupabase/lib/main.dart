32bfcccdfc8dd54d0c1ac726cd9dd7bab9370f78
トム・クルーズ <ryu1421magic@icloud.com>
2024-04-23 14:12:24 +0900

    import supabase

--- testsupabase/lib/main.dart
+++ testsupabase/lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://fngpekjyvukjhnwyfcdf.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZuZ3Bla2p5dnVramhud3lmY2RmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM4NDc3MjIsImV4cCI6MjAyOTQyMzcyMn0.2Y9SK7QKY4Z4oBG6WBQ7dm2w49V3VeVR-GVM4l0vj3Y',
+  );
+
   runApp(const MyApp());
 }
 
